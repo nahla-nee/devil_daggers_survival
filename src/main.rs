c6920807dc9f35116bf8b23b4ed04cb2f874db3f
zee-mzha <zee.mzha@gmail.com>
2021-11-02 13:58:13 -0600

    removed test code

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,5 @@
 mod spawnset;
 mod byte_reader;
 
-use std::path::Path;
-use spawnset::Spawnset;
-
 fn main() {
-	let spawnset = Spawnset::read_from_file(Path::new("/home/zee/storage1/SteamLibrary/steamapps/common/devildaggers/dd/survival"));
-	spawnset.print();
 }
